a86952f272675860d202aaabb553e6f9ee9b6f44
FitsumMehari <m.fitsummehari@gmail.com>
2024-05-05 17:44:55 +0300

    add route for available flights

--- routes/flights.js
+++ routes/flights.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Flight = require("../models/Flight");
+const AvailableFlights = require("../models/AvailableFlight");
 
 // Get all flights
 router.get("/", async(req, res, next) => {
@@ -7,6 +8,54 @@ router.get("/", async(req, res, next) => {
         await Flight.find().then((flights) => {
             res.status(200).json(flights);
         });
+    } catch (error) {
+        return next(error);
+    }
+});
+
+// Get available flights
+router.get("/available", async(req, res, next) => {
+    try {
+        await AvailableFlights.find().then((flights) => {
+            console.log(flights);
+            res.status(200).json(flights);
+        });
+    } catch (error) {
+        return next(error);
+    }
+});
+
+// Add flight
+router.post("/", async(req, res, next) => {
+    const newAvaialableFlight = new AvailableFlights({
+        source: req.body.source,
+        destination: req.body.destination,
+    });
+    try {
+        const addedFlight = newAvaialableFlight.save();
+        res.status(201).json(addedFlight);
+    } catch (error) {
+        return next(error);
+    }
+});
+
+// Reserve flight
+router.post("/reserve", async(req, res, next) => {
+    const flightToReserve = new Flight({
+        firstName: req.body.firstName,
+        middleName: req.body.firstName,
+        lastName: req.body.firstName,
+        email: req.body.firstName,
+        phone: req.body.middleName,
+        address: req.body.lastName,
+        source: req.body.source,
+        destination: req.body.destination,
+        departureDate: req.body.departureDate,
+        departureTime: req.body.departureTime,
+    });
+    try {
+        const reservedFlight = flightToReserve.save();
+        res.status(201).json(reservedFlight);
     } catch (error) {
         return next(error);
     }
