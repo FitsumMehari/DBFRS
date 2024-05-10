0f1f840b9d35b72b6597b275f195b305cf20ca5b
FitsumMehari <m.fitsummehari@gmail.com>
2024-05-10 12:28:01 +0300

    finish all routes of customer

--- routes/customer.js
+++ routes/customer.js
@@ -33,16 +33,49 @@ router.post("/", async(req, res, next) => {
     }
 });
 
+// update customer
+
+router.put("/", async(req, res, next) => {
+    const customerId = req.body.customerId;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const passportId = req.body.passportId;
+    const address = req.body.address;
+    const dateOfBirth = req.body.dateOfBirth;
+    const gender = req.body.gender;
+    const phoneNumber = req.body.phoneNumber;
+
+    try {
+        await Customer.findOneAndUpdate({ customerId: customerId }, {
+            $set: {
+                customerId: customerId,
+                firstName: firstName,
+                lastName: lastName,
+                passportId: passportId,
+                address: address,
+                dateOfBirth: dateOfBirth,
+                gender: gender,
+                phoneNumber: phoneNumber,
+            },
+        });
+
+        res.status(200).json({ message: "update successful" });
+    } catch (error) {
+        next(error);
+    }
+});
+
 // search customer
 
-router.get("/:customerId", async(req, res, next) => {
-    const customerId = req.params.customerId;
+router.get("/:id", async(req, res, next) => {
+    const customerId = req.params.id.split(":")[1];
+    console.log(customerId);
     try {
-        const customer = await Customer.findOne({ customerId: customerId });
+        const customer = await Customer.find({ customerId: customerId });
         if (customer == null) {
             return res.status(404).json({ message: "No Customer Found!" });
         } else {
-            return res.status(200).json(customer);
+            return res.status(200).json(customer[0]);
         }
     } catch (error) {}
 });
