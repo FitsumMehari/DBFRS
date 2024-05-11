d9675649537e47c3c1c596ef8ece0da5a4ac8587
FitsumMehari <m.fitsummehari@gmail.com>
2024-05-11 10:35:05 +0300

    add route to get all flights

--- routes/flight.js
+++ routes/flight.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const Flight = require("../models/Flight");
 
-// add customer
+// add flight
 router.post("/", async(req, res, next) => {
     const flightId = req.body.flightId;
     const flightName = req.body.flightName;
@@ -33,51 +33,31 @@ router.post("/", async(req, res, next) => {
     }
 });
 
-// update customer
+// search flight
 
-router.put("/", async(req, res, next) => {
-    const customerId = req.body.customerId;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const passportId = req.body.passportId;
-    const address = req.body.address;
-    const dateOfBirth = req.body.dateOfBirth;
-    const gender = req.body.gender;
-    const phoneNumber = req.body.phoneNumber;
+// router.get("/:id", async(req, res, next) => {
+//     const customerId = req.params.id.split(":")[1];
+//     console.log(customerId);
+//     try {
+//         const customer = await Customer.find({ customerId: customerId });
+//         if (customer == null) {
+//             return res.status(404).json({ message: "No Customer Found!" });
+//         } else {
+//             return res.status(200).json(customer[0]);
+//         }
+//     } catch (error) {}
+// });
 
+// get all flights
+
+router.get("/", async(req, res, next) => {
     try {
-        await Customer.findOneAndUpdate({ customerId: customerId }, {
-            $set: {
-                customerId: customerId,
-                firstName: firstName,
-                lastName: lastName,
-                passportId: passportId,
-                address: address,
-                dateOfBirth: dateOfBirth,
-                gender: gender,
-                phoneNumber: phoneNumber,
-            },
+        await Flight.find().then((allFlights) => {
+            res.status(200).json(allFlights);
         });
-
-        res.status(200).json({ message: "update successful" });
     } catch (error) {
         next(error);
     }
-});
-
-// search customer
-
-router.get("/:id", async(req, res, next) => {
-    const customerId = req.params.id.split(":")[1];
-    console.log(customerId);
-    try {
-        const customer = await Customer.find({ customerId: customerId });
-        if (customer == null) {
-            return res.status(404).json({ message: "No Customer Found!" });
-        } else {
-            return res.status(200).json(customer[0]);
-        }
-    } catch (error) {}
 });
 
 module.exports = router;
