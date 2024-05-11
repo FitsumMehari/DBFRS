20eeb18d3b754cbc8ef4926c69cc9df2e22f12ef
FitsumMehari <m.fitsummehari@gmail.com>
2024-05-11 12:16:27 +0300

    add entities on the ticket schema

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const TicketSchema = new mongoose.Schema({
+    customerId: {
+        type: String,
+        required: true,
+    },
     firstName: {
         type: String,
         required: true,
