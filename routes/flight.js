64ec7fc1ec692b140b4b75e8d351eb9f51451ec4
FitsumMehari <m.fitsummehari@gmail.com>
2024-05-29 09:12:08 +0300

    add route to search and edit flights

--- routes/flight.js
+++ routes/flight.js
@@ -33,19 +33,34 @@ router.post("/", async(req, res, next) => {
     }
 });
 
+// Get flights
 
-router.get("/", async(req, res, next) => {
+router.get("/:id", async(req, res, next) => {
+    if (req.params.id == "all") {
+        try {
+            await Flight.find().then((allFlights) => {
+                res.status(200).json(allFlights);
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+    const flightId = req.params.id.split(":")[1];
+
     try {
-        await Flight.find().then((allFlights) => {
-            res.status(200).json(allFlights);
-        });
+        const flight = await Flight.find({ flightId: flightId });
+        if (flight == null) {
+            return res.status(404).json({ message: "No flights found!" })
+        } else {
+            return res.status(200).json(flight[0]);
+        }
     } catch (error) {
         next(error);
     }
 });
 
-// get single flight
-
+// add single flight
+// not sure if this ever gets called
 router.post("/single", async(req, res, next) => {
     const source = req.body.source;
     const destination = req.body.destination;
@@ -65,4 +80,47 @@ router.post("/single", async(req, res, next) => {
     }
 });
 
+// update flight
+
+router.put("/", async(req, res, next) => {
+    const flightId = req.body.flightId;
+    const flightName = req.body.flightName;
+    const price = req.body.price;
+    const source = req.body.source;
+    const destination = req.body.destination;
+    const departureDate = req.body.departureDate;
+    const arrivalTime = req.body.arrivalTime;
+    const departureTime = req.body.departureTime;
+
+    try {
+        let foundFlight = await Flight.findOneAndUpdate({ flightId: flightId }, {
+            $set: {
+                flightId: flightId,
+                flightName: flightName,
+                price: price,
+                source: source,
+                destination: destination,
+                departureDate: departureDate,
+                arrivalTime: arrivalTime,
+                departureTime: departureTime,
+            },
+        });
+        res.status(202).json({ message: "update successful" });
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+// delete flight
+
+router.delete("/:id", async(req, res, next) => {
+    const flightId = req.params.id.split(":")[1];
+    try {
+        const result = await Flight.findOneAndDelete({ flightId: flightId })
+        res.status(200);
+    } catch (error) {
+        next(error);
+    }
+})
 module.exports = router;
